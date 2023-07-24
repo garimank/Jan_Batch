ce01c1355b3b5db63e22930385ae7c1046d42e7f
garim <garim@DESKTOP-2IJ7N67>
2023-07-24 16:57:43 +0530

    second code push

---
+++ Project1/src/ToCheckGit/Hello.java
@@ -0,0 +1,13 @@
+package ToCheckGit;
+
+
+public class Hello {
+	public void abc() {
+		System.out.println(" forgit example");
+	}
+	public static void main(String[] args) {
+		Hello obj=new Hello();
+				obj.abc();
+	}
+
+}
